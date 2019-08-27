fcd7d213e422fabd76476bc1d11e4857202d5fb1
Calvin McDowell <calvinmac40@hotmail.com>
2019-08-27 01:55:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,22 @@
 class Dog
-  def name=(dogs_name)
-    @name = dogs_name
-  end
+  attr_accessor :name
+#   def name=(dogs_name)
+#     @name = dogs_name
+#   end
   
-  def name 
-    @name 
-  end
+#   def name 
+#     @name 
+#   end
   
-end
+# end
 
-class Dog
+# class Dog
   def bark
     puts "Woof!"
   end
 end
 
 fido = Dog.new
-fido.bark
+
 
 
